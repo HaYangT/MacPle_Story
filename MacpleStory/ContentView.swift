408cbb51efb122807dfee6b6a70209e4c2f42d8f
HaYangT <juliago00@naver.com>
2026-06-12 15:15:34 +0900

    Initial Commit

---
+++ MacpleStory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacpleStory
+//
+//  Created by 고혜역 on 6/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacpleStoryTests/MacpleStoryTests.swift
@@ -0,0 +1,19 @@
+//
+//  MacpleStoryTests.swift
+//  MacpleStoryTests
+//
+//  Created by 고혜역 on 6/12/26.
+//
+
+import Testing
+@testable import MacpleStory
+
+struct MacpleStoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
